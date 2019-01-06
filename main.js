51f1d34a1f61b6c76d7ce8388c6b65e831ea6b56
ivan <ivanneurbaczka@gmail.com>
2019-01-06 18:19:09 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+let secondes = 0;
+let minutes = 0;
+let heures = 0;
+let addbar = 0;
+
+let chronoSecondes = document.querySelector("#chronosecondes");
+let chronoMinutes = document.querySelector("#chronominutes");
+let chronoHeures = document.querySelector("#chronoheures");
+
+let bar = document.querySelector("#bar");
+
+
+
+
+
+
+
+let start = document.querySelector("#starter");
+
+let stop = document.querySelector("#stoper");
+
+
+
+
+
+ start.addEventListener('click',function(){
+    var starterChrono = setInterval(function(){
+        chronoSecondes.textContent = `${secondes} Secondes`; 
+        bar.style.width = addbar + "px";
+        addbar += 20;
+        secondes++;
+        if(secondes == 59){
+         addbar = 0;
+         minutes++;
+         secondes = 0;
+         chronoMinutes.textContent = `${minutes}  Minutes`;
+        }
+         else if(minutes == 59){
+          minutes = 0;
+          heures++;
+          chronoHeures.textContent = `${heures}  Heures`;
+         }
+    },1000)
+
+
+    stop.addEventListener('click',function(){
+        clearInterval(starterChrono);
+    })
+    
+})
+
+
+
+
